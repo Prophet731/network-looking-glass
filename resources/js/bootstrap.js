4f7e2591a77f63292b7dfee41d58e9ec95a7d652
Andrew Murray <andrew@amurray.me>
2023-11-06 19:08:48 -0500

    Updated bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -28,10 +28,51 @@ window.Echo = new Echo({
     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
+    encrypted: true,
+    disableStats: true,
     enabledTransports: ['ws', 'wss'],
 });
-window.Echo.connector.pusher.connection.bind('connected', function () {
-    window.axios.defaults.headers.common['X-Socket-Id'] = window.Echo.socketId();
+
+window.Echo.connector.pusher.connection.bind('connecting', (payload) => {
+    /**
+     * All dependencies have been loaded and Channels is trying to connect.
+     * The connection will also enter this state when it is trying to reconnect after a connection failure.
+     */
+});
+
+window.Echo.connector.pusher.connection.bind('connected', (payload) => {
+    /**
+     * The connection to Channels is open and authenticated with your app.
+     */
+    window.axios.defaults.headers.common['X-Socket-Id'] = payload.socket_id;
+    console.log('connected', payload);
+});
+
+window.Echo.connector.pusher.connection.bind('unavailable', (payload) => {
+    /**
+     *  The connection is temporarily unavailable. In most cases this means that there is no internet connection.
+     *  It could also mean that Channels is down, or some intermediary is blocking the connection. In this state,
+     *  pusher-js will automatically retry the connection every 15 seconds.
+     */
+});
+
+window.Echo.connector.pusher.connection.bind('failed', (payload) => {
+    /**
+     * Channels is not supported by the browser.
+     * This implies that WebSockets are not natively available and an HTTP-based transport could not be found.
+     */
+});
+
+window.Echo.connector.pusher.connection.bind('disconnected', (payload) => {
+    /**
+     * The Channels connection was previously connected and has now intentionally been closed
+     */
+});
+
+window.Echo.connector.pusher.connection.bind('message', (payload) => {
+    /**
+     * Ping received from server
+     */
 });
 
 window.app = {
